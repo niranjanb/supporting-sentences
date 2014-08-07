3ad59f4e70b2074a2e0be224e5219d4a9e0120f3
Ashish Sabharwal <AshishS@allenai.org>
2014-08-07 16:19:31 -0700

    added ARFF file output for weka

--- src/main/scala/org/allenai/ari/sentences/FeatureGenerator.scala
+++ src/main/scala/org/allenai/ari/sentences/FeatureGenerator.scala
@@ -6,26 +6,28 @@ import java.io.PrintWriter
 
 import scala.io.Source
 
-/**
- * Created by niranjan on 8/7/14.
- */
+
+/** Created by niranjan on 8/7/14.
+  */
 case class QuestionSentence(qid: String, question: String, focus: String, url: String, sentence: String, annotation: Int)
 
 case class Instance(questionSentence: QuestionSentence, features: Map[String, Double]) {
-  override  def toString(): String = {
+  override def toString(): String = {
     questionSentence.toString + "\t" + features.values.mkString("\t")
+  }
+
+  def toARFF(): String = {
+    features.values.mkString(",") + "," + (questionSentence.annotation > 0)
   }
 }
 
-
 object FeatureGenerator extends App {
-
 
   //private val config = ConfigFactory.load()
   val inputFile = args(0)
   val outputFile = args(1)
 
-  val questionSentences = Source.fromFile(inputFile).getLines().map {
+  val questionSentences = Source.fromFile(inputFile).getLines().drop(1).map {
     line =>
       //Assume line is of the following form:
       //Annotator	Q ID	T/F question	Focus	URL	Sentence	Supporting (0-2)?	Necessary rewrite
@@ -35,30 +37,47 @@ object FeatureGenerator extends App {
       QuestionSentence(splits(0), splits(1), splits(2), splits(3), splits(4), splits(5).toInt)
   }
 
-  def overlap(src: String, tgt: String) = {
-    val srcKeywords = Tokenizer.toKeywords(src)
-    val tgtKeywords = Tokenizer.toKeywords(tgt)
-    srcKeywords.intersect(tgtKeywords).size / tgtKeywords.size
-  }
-
-  def features(questionSentence: QuestionSentence) = {
-    var features = Map[String, Double]()
-    features += ("sentence-length" -> questionSentence.sentence.length.toDouble)
-    //add more features.
-    features += ("word-overlap" -> overlap(questionSentence.question, questionSentence.sentence))
-    features
-  }
-
   val instances: Iterator[Instance] = questionSentences.map {
     questionSentence => Instance(questionSentence, features(questionSentence))
   }
 
-  val writer = new PrintWriter(outputFile)
+  toARFF(outputFile)
 
-  instances.foreach{
-    instance =>
-      writer.println(instance.toString)
+  def overlap(src: String, tgt: String, asFrac: Boolean) = {
+    val srcKeywords = Tokenizer.toKeywords(src)
+    val tgtKeywords = Tokenizer.toKeywords(tgt)
+    if (asFrac)
+      Math.round(srcKeywords.intersect(tgtKeywords).size / tgtKeywords.size.toDouble * 1000000.0) / 1000000.0
+    else
+      srcKeywords.intersect(tgtKeywords).size
   }
-  writer.close()
+
+  def features(questionSentence: QuestionSentence) = {
+    var features = Map[String, Double]()
+    // number of words in the sentence
+    features += ("sentence-length" -> questionSentence.sentence.split("\\s+").size)
+    // number of sentence words that overlap with the question
+    features += ("word-overlap-num" -> overlap(questionSentence.question, questionSentence.sentence, false))
+    // fraction of sentence words that overlap with the question
+    features += ("word-overlap-frac" -> overlap(questionSentence.question, questionSentence.sentence, true))
+    features
+  }
+
+  def toARFF(arffFile: String) = {
+    val writer = new PrintWriter(arffFile)
+    // add ARFF header
+    writer.println("@relation SENTENCE_SELECTOR")
+    writer.println("  @attribute sentence-length       numeric         % length of the sentence")
+    writer.println("  @attribute word-overlap-num      numeric         % number of sentence words that overlap the question")
+    writer.println("  @attribute word-overlap-frac     numeric         % fractino of sentence words that overlap the question")
+    writer.println("  @attribute class                 {true,false}    % BINARY LABEL: whether the sentence supports the question")
+    writer.println("")
+    // add ARFF data
+    writer.println("@data")
+    instances.foreach {
+      instance => writer.println(instance.toARFF)
+    }
+    writer.close()
+  }
 
 }
