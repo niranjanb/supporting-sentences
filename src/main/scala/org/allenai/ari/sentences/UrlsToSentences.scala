efc95e1fca7dab93d0369edbe25ca69b4ea34621
Niranjan Balasubramanian <niranjan@lovelace.local>
2014-11-20 10:12:05 -0800

    Minor code cleanup

--- src/main/scala/org/allenai/ari/sentences/UrlsToSentences.scala
+++ src/main/scala/org/allenai/ari/sentences/UrlsToSentences.scala
@@ -10,56 +10,24 @@ import scala.collection.mutable
 import scala.io.Source
 import scala.util.Try
 
+case class QueryUrl(query: String, urlIndex: String, url: String)
+
 object UrlsToSentences extends App with Logging {
-
-  case class QueryUrl(query: String, urlIndex: String, url: String) {}
-
-  def crawl(url: String) = Try {
-    Source.fromURL(url).getLines().mkString
-  } getOrElse {
-    logger.error(s"Failed to download url $url")
-    ""
-  }
-
-  def writeContent(outputDirectory: String, queryUrl: QueryUrl, content: String) = {
-    val htmlFile = s"${outputDirectory}/${queryUrl.query}-${queryUrl.urlIndex}.html"
-    val htmlWriter = new PrintWriter(htmlFile, "UTF-8")
-    logger.info(s"Url content size ${content.size} starting with ${content.substring(0, math.min(100, content.length))}")
-    htmlWriter.write(content)
-    htmlWriter.close()
-  }
-  
-  def writeSentences(outputDirectory: String, queryUrl: QueryUrl, sentences: mutable.WrappedArray[String]) {
-    val sentencesFile = s"$outputDirectory/${queryUrl.query}-${queryUrl.urlIndex}.txt"
-    val sentenceWriter = new PrintWriter(sentencesFile, "UTF-8")
-    sentences.foreach {
-      sentence =>
-        sentenceWriter.println(queryUrl.query + "\t" + sentence + "\t" + queryUrl.url)
-    }
-    sentenceWriter.close()
-  }
-
-  def toText(html: String) = Jsoup.parse(html).text()
-
-  val sentencer = new OpenNlpSentencer()
-  def toSentences(text: String) = sentencer.segmentTexts(text)
 
   val inputFile = args(0)
   val outputDirectory = args(1)
-
-  Source.fromFile(inputFile, "UTF-8").getLines().foreach {
+  Source.fromFile(inputFile, "UTF-8").getLines() foreach {
     line =>
       try {
-        logger.info(s"Processing line $line")
-        val splits = line.split("\t")
-        if (splits.size > 2) {
-          logger.info("Url: " + splits(2))
-          val queryUrl = QueryUrl(splits(0), splits(1), splits(2))
-          val content = crawl(queryUrl.url)
-          writeContent(outputDirectory, queryUrl, content)
-          val sentences = toSentences(toText(content))
-          logger.info(s"# sentences ${sentences.size}")
-          writeSentences(outputDirectory, queryUrl, sentences)
+        logger.debug(s"Processing $line")
+        line.split("\t") match {
+          case Array(query: String, urlIndex: String, url: String) =>
+            val queryUrl = QueryUrl(query, urlIndex, url)
+            val content = crawl(queryUrl.url)
+            saveHtmlToFile(outputDirectory, queryUrl, content)
+            saveSentencesToFile(outputDirectory, queryUrl, toSentences(toText(content)))
+          case _ =>
+            logger.warn(s"Skipping $line")
         }
       } catch {
         case e:Exception =>
@@ -67,4 +35,32 @@ object UrlsToSentences extends App with Logging {
           logger.error(e.getStackTraceString)
       }
   }
+  def crawl(url: String) = Try {
+    Source.fromURL(url).getLines().mkString
+  } getOrElse {
+    logger.error(s"Failed to download url $url")
+    ""
+  }
+  def saveHtmlToFile(outputDirectory: String, queryUrl: QueryUrl, content: String) = {
+    val htmlFile = s"${outputDirectory}/${queryUrl.query}-${queryUrl.urlIndex}.html"
+    val writer = new PrintWriter(htmlFile, "UTF-8")
+    writer.write(content)
+    writer.close()
+  }
+
+  def saveSentencesToFile(outputDirectory: String, queryUrl: QueryUrl, sentences: mutable.WrappedArray[String]) {
+    val sentencesFile = s"$outputDirectory/${queryUrl.query}-${queryUrl.urlIndex}.txt"
+    val writer = new PrintWriter(sentencesFile, "UTF-8")
+    sentences.foreach {
+      sentence =>
+        writer.println(s"${queryUrl.query}\t$sentence\t${queryUrl.url}")
+    }
+    writer.close()
+  }
+
+  def toText(html: String) = Jsoup.parse(html).text()
+
+  val sentencer = new OpenNlpSentencer()    
+  def toSentences(text: String) = sentencer.segmentTexts(text)
+
 }
